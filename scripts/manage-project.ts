a4f071876f64656a56ff870f12193e29b3acf196
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-10-17 15:14:56 +0200

    Update manage-project.ts

--- scripts/manage-project.ts
+++ scripts/manage-project.ts
@@ -4,14 +4,16 @@ import colors from "colors";
 import { exec } from "child_process";
 import { buildMdTablesFromOpenApi } from "./build-md-tables-from-openapi";
 import { updateMdTablesInDoc } from "./update-md-tables-in-doc";
+import { boolean } from "yup";
 
 dotenv.config();
 const options = minimist(process.argv.slice(2));
 const versionTag = options.versionTag || options.vt;
 const versionOption = options.version || options.v;
+
+//globals
 const { create, update } = options;
 const help = options.help || options.h;
-
 const mainVersion = "v2018-08-01";
 const version =
   versionOption || versionTag ? `${mainVersion}-${versionTag}` : undefined;
@@ -28,54 +30,101 @@ const listOfGuideCategories = [
 
 const listOfReferenceCategories = ["Introduction"];
 
-const main = async () => {
-  if (help) {
-    console.log(
-      colors.green(
-        `options:` +
-          `\n"versionTag" or "vt" for versionTag` +
-          `\n"version" or "v" for version` +
-          `\n"create" if you want to create such version` +
-          `\n"update" if you want to update such version` +
-          `\n\nversionTag or version is required!` +
-          `\ncreate or update option is required!` +
-          `\n\nexamples:` +
-          `\nnpm run manage-project -- --vt=piotr-123 --create` +
-          `\nnpm run manage-project -- --v=v2018-08-01-piotr-123 --create` +
-          `\nnpm run manage-project -- --vt=piotr-123 --update` +
-          `\nnpm run manage-project -- --v=v2018-08-01-piotr-123 --update`
-      )
-    );
-    return;
-  }
-  if (!version) {
-    console.log(
-      colors.red(
-        "invalid arguments, missing `version` or `versionTag`, check `help` for more information\nrun 'npm run manage-project -- --help'"
-      )
-    );
-    return;
-  }
-  if (!create && !update) {
-    console.log(
-      colors.red(
-        "invalid arguments, missing `update` or `create`, check `help` for more information\nrun 'npm run manage-project -- --help'"
-      )
-    );
-    return;
-  }
-  if (create && update) {
-    console.log(
-      colors.red(
-        "invalid arguments, you provided conflicting arguments `update` and `create`, check `help` for more information\nrun 'npm run manage-project -- --help'"
-      )
-    );
+const main = async ({
+  help,
+  version,
+  create,
+  update,
+}: {
+  help?: boolean;
+  version?: string;
+  create?: boolean;
+  update?: boolean;
+}) => {
+  const valid = validateOptions({ help, version, create, update });
+  if (!valid) {
     return;
   }
   if (create) {
     await createNewVersion(version);
   }
   await cleanProject(version);
+  await uploadOpenApiFile(version);
+  await buildMdTables();
+  await updateMdTablesInDocs();
+  await uploadGuideFiles(version);
+  await uploadReferenceDocsWithMaxNumberOfAttempts(version);
+  console.log(
+    colors.green(`\n\nDONE!\nVisit: https://docs.voucherify.io/${version}/`)
+  );
+};
+
+const uploadReferenceDocsWithMaxNumberOfAttempts = async (
+  version,
+  maxNumberOfUploadingAttempts = 6,
+  timeoutAfterEachFail = 5000
+) => {
+  console.log(colors.green("UPLOADING REFERENCE DOC FILES..."));
+  for (let i = 1; i <= maxNumberOfUploadingAttempts; i++) {
+    const success = await updateReferenceDocs(version);
+    if (success) {
+      console.log(colors.green("REFERENCE DOC FILES WERE UPLOADED!"));
+      break;
+    }
+    if (i === maxNumberOfUploadingAttempts) {
+      throw new Error("REFERENCE DOC WERE NOT UPLOADED!");
+    }
+    await new Promise((r) => setTimeout(r, timeoutAfterEachFail));
+  }
+};
+
+const uploadGuideFiles = async (version) => {
+  console.log(colors.green("UPLOADING GUIDES DOC FILES..."));
+  await new Promise((resolve) => {
+    exec(
+      `rdme docs ./docs/guides --version=${version}`,
+      (error, stdout, stderr) => {
+        if (stdout?.includes("successfully created")) {
+          console.log(colors.green("GUIDES DOC FILES WERE UPLOADED!"));
+          return resolve(true);
+        }
+        console.log(colors.red(error?.toString?.()));
+        throw new Error("GUIDES DOC WERE NOT UPLOADED!");
+      }
+    );
+  });
+};
+
+const updateMdTablesInDocs = async () => {
+  console.log(colors.green("UPDATING MD TABLES IN DOCS..."));
+  await new Promise((resolve) => {
+    exec(`npm run update-md-tables-in-doc`, (error, stdout, stderr) => {
+      if (stdout) {
+        console.log(
+          colors.green("MD TABLES WERE UPDATED IN DOCS SUCCESSFULLY!")
+        );
+        return resolve(true);
+      }
+      console.log(colors.red(error?.toString?.()));
+      throw new Error("MD TABLES WERE NOT UPDATED IN DOCS!");
+    });
+  });
+};
+const buildMdTables = async () => {
+  console.log(colors.green("BUILDING MD TABLES FROM OPEN API..."));
+  await new Promise((resolve) => {
+    exec(`npm run build-md-tables-from-openapi`, (error, stdout, stderr) => {
+      if (stdout) {
+        console.log(colors.green("MD TABLES WERE BUILDED SUCCESSFULLY!"));
+        return resolve(true);
+      }
+      console.log(colors.red(error?.toString?.()));
+      throw new Error("MD TABLES WERE NOT BUILDED!");
+    });
+  });
+};
+
+const uploadOpenApiFile = async (version) => {
   console.log(
     colors.green(
       "UPLOADING OPEN API FILE... PLEASE WAIT... THIS MAY TAKE UP TO A MINUTE"
@@ -102,60 +151,48 @@ const main = async () => {
       }
     );
   });
-  console.log(colors.green("BUILDING MD TABLES FROM OPEN API..."));
-  await new Promise((resolve) => {
-    exec(`npm run build-md-tables-from-openapi`, (error, stdout, stderr) => {
-      if (stdout) {
-        console.log(colors.green("MD TABLES WERE BUILDED SUCCESSFULLY!"));
-        return resolve(true);
-      }
-      console.log(colors.red(error?.toString?.()));
-      throw new Error("MD TABLES WERE NOT BUILDED!");
-    });
-  });
-  console.log(colors.green("UPDATING MD TABLES IN DOCS..."));
-  await new Promise((resolve) => {
-    exec(`npm run update-md-tables-in-doc`, (error, stdout, stderr) => {
-      if (stdout) {
-        console.log(
-          colors.green("MD TABLES WERE UPDATED IN DOCS SUCCESSFULLY!")
-        );
-        return resolve(true);
-      }
-      console.log(colors.red(error?.toString?.()));
-      throw new Error("MD TABLES WERE NOT UPDATED IN DOCS!");
-    });
-  });
-  console.log(colors.green("UPLOADING GUIDES DOC FILES..."));
-  await new Promise((resolve) => {
-    exec(
-      `rdme docs ./docs/guides --version=${version}`,
-      (error, stdout, stderr) => {
-        if (stdout?.includes("successfully created")) {
-          console.log(colors.green("GUIDES DOC FILES WERE UPLOADED!"));
-          return resolve(true);
-        }
-        console.log(colors.red(error?.toString?.()));
-        throw new Error("GUIDES DOC WERE NOT UPLOADED!");
-      }
-    );
-  });
-  console.log(colors.green("UPLOADING REFERENCE DOC FILES..."));
-  const maxNumberOfUploadingAttempts = 3;
-  for (let i = 1; i <= maxNumberOfUploadingAttempts; i++) {
-    const success = await updateReferenceDocs(version);
-    if (success) {
-      console.log(colors.green("REFERENCE DOC FILES WERE UPLOADED!"));
-      break;
-    }
-    if (i === maxNumberOfUploadingAttempts) {
-      throw new Error("REFERENCE DOC WERE NOT UPLOADED!");
-    }
-    await new Promise((r) => setTimeout(r, 10000));
+};
+
+const validateOptions = ({
+  help,
+  version,
+  create,
+  update,
+}: {
+  help?: boolean;
+  version?: string;
+  create?: boolean;
+  update?: boolean;
+}) => {
+  if (help || (!version && !create && !update)) {
+    printHelp();
+    return false;
   }
-  console.log(
-    colors.green(`\n\nDONE!\nVisit: https://docs.voucherify.io/${version}/`)
-  );
+  if (!version) {
+    console.log(
+      colors.red(
+        "invalid arguments, missing `version` or `versionTag`, check `help` for more information\nrun 'npm run manage-project -- --help'"
+      )
+    );
+    return false;
+  }
+  if (!create && !update) {
+    console.log(
+      colors.red(
+        "invalid arguments, missing `update` or `create`, check `help` for more information\nrun 'npm run manage-project -- --help'"
+      )
+    );
+    return false;
+  }
+  if (create && update) {
+    console.log(
+      colors.red(
+        "invalid arguments, you provided conflicting arguments `update` and `create`, check `help` for more information\nrun 'npm run manage-project -- --help'"
+      )
+    );
+    return false;
+  }
+  return true;
 };
 
 const updateReferenceDocs = async (version) => {
@@ -329,4 +366,28 @@ const asyncMap = (arr, asyncFn) => {
   return Promise.all(arr.map(asyncFn));
 };
 
-main();
+const printHelp = () => {
+  console.log(
+    colors.green(
+      `options:` +
+        `\n"versionTag" or "vt" for versionTag` +
+        `\n"version" or "v" for version` +
+        `\n"create" if you want to create such version` +
+        `\n"update" if you want to update such version` +
+        `\n\nversionTag or version is required!` +
+        `\ncreate or update option is required!` +
+        `\n\nexamples:` +
+        `\nnpm run manage-project -- --vt=piotr-123 --create` +
+        `\nnpm run manage-project -- --v=v2018-08-01-piotr-123 --create` +
+        `\nnpm run manage-project -- --vt=piotr-123 --update` +
+        `\nnpm run manage-project -- --v=v2018-08-01-piotr-123 --update`
+    )
+  );
+};
+
+main({
+  help,
+  version,
+  create,
+  update,
+});
