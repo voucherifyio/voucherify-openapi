571cc208359d089a4b70c198d9570f5c0a319456
jkaliszuk <jakub.kaliszuk@rspective.pl>
2023-11-03 11:47:23 +0100

    Run script - md tables, formatting.

--- scripts/md-tables.ts
+++ scripts/md-tables.ts
@@ -22,7 +22,7 @@ export const mdTables: [string, string?][] = [
     ['13_obj_validation_rule_object', 'VALIDATION-RULES-API-Validation-Rule-Object.md'],
     ['validation_rule_assignment', 'VALIDATION-RULES-API-Validation-Rule-Assignment-Object.md'],
     ['14_obj_segment_object', 'SEGMENTS-API-Customer-Segment-Object.md'],
-    ['15_req_track_custom_event', 'EVENTS-API-Custom-Event-Object.md'],
+    ['TrackCustomEventRequestBody', 'EVENTS-API-Custom-Event-Object.md'],
     ['18_res_list_consents_GET', 'CONSENTS-API-Consents-Object.md'],
     ['17_obj_async_action_object', 'ASYNC-ACTIONS-API-Async-Action-Object.md'],
     ['Export', 'EXPORTS-API-Export-Object.md'],
