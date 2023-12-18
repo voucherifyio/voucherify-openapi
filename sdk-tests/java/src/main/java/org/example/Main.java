f07205ecf81d2e3f40aaad8f8f9c7ff79608c1ad
jkaliszuk <jakub.kaliszuk@rspective.pl>
2023-12-18 17:33:27 +0100

    First test program for sdk java (not working quite yet).

---
+++ sdk-tests/java/src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.openapitools.client.ApiClient;
+import org.openapitools.client.ApiException;
+import org.openapitools.client.Configuration;
+import org.openapitools.client.api.CampaignsApiApi;
+import org.openapitools.client.auth.ApiKeyAuth;
+import org.openapitools.client.model.CampaignsAddVoucherWithSpecificCodeRequestBody;
+import org.openapitools.client.model.CampaignsAddVoucherWithSpecificCodeResponseBody;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath("https://dev.api.voucherify.io");
+
+        ApiKeyAuth id = (ApiKeyAuth) defaultClient.getAuthentication("X-App-Id-1");
+        id.setApiKey(dotenv.get("APPLICATION_ID"));
+
+        ApiKeyAuth token = (ApiKeyAuth) defaultClient.getAuthentication("X-App-Token-1");
+        token.setApiKey(dotenv.get("SECRET_KEY"));
+
+        CampaignsApiApi apiInstance = new CampaignsApiApi(defaultClient);
+
+        String campaignId = "camp_vI2UgSzd7yamAFrFYe4vEik4";
+        String code = "5ad31sf";
+
+        CampaignsAddVoucherWithSpecificCodeRequestBody campaignsAddVoucherWithSpecificCodeRequestBody = new CampaignsAddVoucherWithSpecificCodeRequestBody();
+
+        try {
+            CampaignsAddVoucherWithSpecificCodeResponseBody result = apiInstance.addVoucherWithSpecificCodeToCampaign(campaignId, code, campaignsAddVoucherWithSpecificCodeRequestBody);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling CampaignsApiApi#addVoucherWithSpecificCodeToCampaign");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+    }
+}
