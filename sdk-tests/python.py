1a6f5c236925cd1a8d19cbdfc6ed1a81f492f3cd
jkaliszuk <jakub.kaliszuk@rspective.pl>
2024-01-02 16:31:12 +0100

    Added test_add_vouchers_to_campaign to python tests. Fixed CodeConfigRequiredLengthCharsetPattern length to number.

--- sdk-tests/python.py
+++ sdk-tests/python.py
@@ -28,6 +28,45 @@ class TestYourSDK(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestYourSDK, self).__init__(*args, **kwargs)
 
+    @responses.activate
+    def test_add_vouchers_to_campaign(self):
+        with openapi_client.ApiClient(configuration) as api_client:
+            api_instance = openapi_client.CampaignsApi(api_client)
+
+            randomCode = random.randint(0, 100000)
+            campaign_name = "camp_test_" + str(randomCode)
+            code = "test_code_" + str(randomCode)
+
+            try:
+                campaignsCreateRequestBody = openapi_client.CampaignsCreateRequestBody(
+                    actual_instance=openapi_client.CampaignsCreateDiscountCouponsCampaign(
+                        name=campaign_name,
+                        voucher=openapi_client.DiscountCouponsCampaignVoucher(
+                            type="DISCOUNT_VOUCHER",
+                            discount=openapi_client.Discount(
+                                actual_instance=openapi_client.DiscountAmount(
+                                    type="AMOUNT",
+                                    amount_off=1000
+                                )
+                            )
+                        ),
+                        metadata={
+                            "mandatory_v": "test"
+                        }
+                    )
+                )
+                campaign = api_instance.create_campaign(campaignsCreateRequestBody)
+
+                async_action = api_instance.add_vouchers_to_campaign(campaign.id, 5)
+                campaigns_vouchers_create_response_body = api_instance.add_vouchers_to_campaign(campaign.id)
+                campaigns_vouchers_specific_code_create_response_body = api_instance.add_voucher_with_specific_code_to_campaign(campaign.id, code)
+
+                pprint(async_action)
+                pprint(campaigns_vouchers_create_response_body)
+                pprint(campaigns_vouchers_specific_code_create_response_body)
+
+            except ApiException as e:
+                print("Exception when calling CAMPAIGNSAPIApi->create_qualification: %s\n" % e)
 
     @responses.activate
     def test_list_redemptions(self):
@@ -35,7 +74,7 @@ class TestYourSDK(unittest.TestCase):
             api_instance = openapi_client.RedemptionsApi(api_client)
 
             try:
-                result = api_instance.list_redemptions(limit=1)
+                result = api_instance.list_redemptions()
 
                 pprint(result)
 
