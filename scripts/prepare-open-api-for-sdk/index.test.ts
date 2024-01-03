fe67dfb8ec832f2d2198493f41ac8a9f279997a9
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2024-01-03 09:56:07 +0100

    requested changes

--- scripts/prepare-open-api-for-sdk/index.test.ts
+++ scripts/prepare-open-api-for-sdk/index.test.ts
@@ -1,6 +1,6 @@
-import fs from 'fs/promises';
-import path from 'path'
-import {exec} from 'child_process' ;
+import fs from "fs/promises";
+import path from "path";
+import { exec } from "child_process";
 
 //this test is only usefully while refactoring `prepare-open-api-for-sdk` script
 //this test is only usefully while refactoring `prepare-open-api-for-sdk` script
@@ -19,37 +19,47 @@ import {exec} from 'child_process' ;
 // and run `jest --findRelatedTests ./scripts/prepare-open-api-for-sdk/index.test.ts`
 
 function execShellCommand(cmd) {
-    return new Promise((resolve, reject) => {
-     exec(cmd, (error, stdout, stderr) => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
       if (error) {
-       console.warn(error);
+        console.warn(error);
       }
-      resolve(stdout? stdout : stderr);
-     });
+      resolve(stdout ? stdout : stderr);
     });
-   }
+  });
+}
 
-describe('compare open-api file', () => {
-  test('ruby', async () => {
-    const openApiPath = path.join(__dirname, '../../tmp/reference/ruby/OpenAPI.json')
-    try{
-      await fs.unlink(openApiPath)
-    }catch(e){
-      // File does not exists
+describe("compare open-api file", () => {
+  test("ruby", async () => {
+    const openApiPath = path.join(
+      __dirname,
+      "../../tmp/reference/ruby/OpenAPI.json"
+    );
+    try {
+      await fs.unlink(openApiPath);
+    } catch (e) {
+      console.log("File does not exists");
     }
-    await execShellCommand('npm run prepare-open-api-for-sdk -- --language=ruby')
-    const openApiSchem = (await fs.readFile(openApiPath)).toString()
+    await execShellCommand(
+      "npm run prepare-open-api-for-sdk -- --language=ruby"
+    );
+    const openApiSchem = (await fs.readFile(openApiPath)).toString();
     expect(openApiSchem).toMatchSnapshot();
   });
-  test('python', async () => {
-    const openApiPath = path.join(__dirname, '../../tmp/reference/python/OpenAPI.json')
-    try{
-      await fs.unlink(openApiPath)
-    }catch(e){
-      // File does not exists
+  test("python", async () => {
+    const openApiPath = path.join(
+      __dirname,
+      "../../tmp/reference/python/OpenAPI.json"
+    );
+    try {
+      await fs.unlink(openApiPath);
+    } catch (e) {
+      console.log("File does not exists");
     }
-    await execShellCommand('npm run prepare-open-api-for-sdk -- --language=python')
-    const openApiSchem = (await fs.readFile(openApiPath)).toString()
+    await execShellCommand(
+      "npm run prepare-open-api-for-sdk -- --language=python"
+    );
+    const openApiSchem = (await fs.readFile(openApiPath)).toString();
     expect(openApiSchem).toMatchSnapshot();
   });
 });
