9894daa8b87e86c1365aaa584463ce0b843fae2e
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2024-01-04 23:29:05 +0100

    Update removeOneOfs.ts

--- scripts/prepare-open-api-for-sdk/removeOneOfs.ts
+++ scripts/prepare-open-api-for-sdk/removeOneOfs.ts
@@ -1,5 +1,6 @@
 import {
   camelCase,
+  compact,
   intersection,
   isEqual,
   omit,
@@ -8,99 +9,149 @@ import {
 } from "lodash";
 import { isDeepStrictEqual } from "util";
 
-const removeSingleOneOf = (schemas) => {
-  const removeOneOf = (schema, schemas, title) => {
-    if (Array.isArray(schema)) {
-      let removed = false;
-      return {
-        removed,
-        schema: schema.map((schemaItem) => {
-          if (removed) {
-            return schemaItem;
-          }
-          const { removed: _removed, schema } = removeOneOf(
-            schemaItem,
+const checkProperties = (properties, title, schemas) => {
+  return Object.fromEntries(
+    Object.entries(properties).map((propertyNameAndEntry) => {
+      const [propertyName, entry] = propertyNameAndEntry as any;
+      if (entry?.oneOf) {
+        return [
+          propertyName,
+          removeOneOf(
+            entry,
             schemas,
-            title
-          );
-          if (_removed) {
-            removed = true;
-          }
-          return schema;
-        }),
+            title + upperFirst(camelCase(propertyName))
+          ),
+        ];
+      }
+      if (entry?.properties) {
+        return [
+          propertyName,
+          {
+            type: "object",
+            properties: checkProperties(
+              (entry as any).properties,
+              title,
+              schemas
+            ),
+          },
+        ];
+      }
+      if (entry.items?.oneOf) {
+        return [
+          propertyName,
+          {
+            type: "array",
+            items: removeOneOf(
+              entry.items,
+              schemas,
+              title + upperFirst(camelCase(propertyName)) + "Item"
+            ),
+          },
+        ];
+      }
+      return [propertyName, entry];
+    })
+  );
+};
+
+const removeOneOf = (schema, schemas, title) => {
+  if (Array.isArray(schema)) {
+    let removed = false;
+    return {
+      removed,
+      schema: schema.map((schemaItem) => {
+        if (removed) {
+          return schemaItem;
+        }
+        const { removed: _removed, schema } = removeOneOf(
+          schemaItem,
+          schemas,
+          title
+        );
+        if (_removed) {
+          removed = true;
+        }
+        return schema;
+      }),
+    };
+  }
+  if (!(schema instanceof Object) || !("oneOf" in schema)) {
+    if (schema.properties) {
+      console.log(88);
+      return {
+        removed: false,
+        schema: {
+          type: "object",
+          properties: checkProperties(schema.properties, title, schemas),
+        },
       };
     }
-    if (!(schema instanceof Object) || !("oneOf" in schema)) {
-      return { removed: false, schema };
-    }
-    const oneOf = schema["oneOf"].map(
-      (data) => schemas[data.$ref?.split("/")?.at(-1)]
-    );
-    if (oneOf.find((item) => !item)) {
-      return { type: "any" };
-    }
-    if (
-      !oneOf ||
-      oneOf.reduce((accumulator, currentValue) => {
-        if (!!currentValue?.oneOf) {
-          return accumulator;
-        }
-        return false;
-      }, false)
-    ) {
-      return { removed: false, schema };
-    }
+    return { removed: false, schema };
+  }
+  const oneOf = schema["oneOf"].map((data) =>
+    data.$ref ? schemas[data.$ref?.split("/")?.at(-1)] : "any"
+  );
+  if (oneOf.find((item) => item === "any")) {
+    return { removed: true, schema: { title: "Any", type: "any" } };
+  }
+  if (
+    !oneOf ||
+    oneOf.reduce((accumulator, currentValue) => {
+      if (!!currentValue?.oneOf) {
+        return accumulator;
+      }
+      return false;
+    }, false)
+  ) {
+    return { removed: false, schema };
+  }
 
-    const oneOfWithNoRefs = oneOf
-      .map((oneOfPartial) => {
-        if (!oneOfPartial?.allOf) {
-          return oneOfPartial;
-        }
-        const allOf = oneOfPartial["allOf"].map((data) => {
-          if (data.$ref?.split("/")?.at(-1)) {
-            return schemas[data.$ref?.split("/")?.at(-1)];
-          }
-          return data;
-        });
-        // console.log(888, allOf);
-        if (
-          allOf.reduce((accumulator, currentValue) => {
-            return !!currentValue?.oneOf;
-          }, false)
-        ) {
-          console.log("error");
-          return { break: true };
-        }
-        return { ...oneOfPartial, allOf };
-      })
-      .map((oneOfPartial) => {
-        if (oneOfPartial?.allOf) {
-          return oneOfPartial.allOf;
-        }
+  const oneOfWithNoRefs = oneOf
+    .map((oneOfPartial) => {
+      if (!oneOfPartial?.allOf) {
         return oneOfPartial;
-      })
-      .flat();
-    if (!oneOfWithNoRefs) {
-      return { type: "any" };
-    }
-    if (oneOfWithNoRefs.find((a) => a?.break)) {
-      return { removed: false, schema };
-    }
-    if (oneOfWithNoRefs.find((a) => !a)) {
-      console.log(123, oneOfWithNoRefs, schema);
-    }
-    return {
-      removed: true,
-      schema: mergeAllOfObjects(oneOfWithNoRefs, schemas, title),
-    };
+      }
+      const allOf = oneOfPartial["allOf"].map((data) => {
+        if (data.$ref?.split("/")?.at(-1)) {
+          return schemas[data.$ref?.split("/")?.at(-1)];
+        }
+        return data;
+      });
+      if (
+        allOf.reduce((accumulator, currentValue) => {
+          return !!currentValue?.oneOf;
+        }, false)
+      ) {
+        console.log("error");
+        return { break: true };
+      }
+      return { ...oneOfPartial, allOf };
+    })
+    .map((oneOfPartial) => {
+      if (oneOfPartial?.allOf) {
+        return oneOfPartial.allOf;
+      }
+      return oneOfPartial;
+    })
+    .flat();
+  if (!oneOfWithNoRefs) {
+    return { type: "any" };
+  }
+  if (oneOfWithNoRefs.find((a) => a?.break)) {
+    console.log(66);
+    return { removed: false, schema };
+  }
+  return {
+    removed: true,
+    schema: mergeAllOfObjects(oneOfWithNoRefs, schemas, title),
   };
-
+};
+const removeSingleOneOf = (schemas) => {
   let removed = false;
   const schemas_ = Object.fromEntries(
     Object.entries(schemas).map((schemaNameAndEntry) => {
       const [schemaName, entry] = schemaNameAndEntry;
       if (removed) {
-        // console.log(1423143);
         return [schemaName, entry];
       }
       const result = removeOneOf(entry, schemas, schemaName);
@@ -130,12 +181,29 @@ const propertiesIntersection = (
     allPropertyKeys.map((key) => {
       const p1 = currentProperties[key];
       const p2 = newProperties[key];
+      // console.log(p1, p2);
       return [
         key,
-        typeIntersection(p1, p2, title + upperFirst(camelCase(key)), schemas),
+        !p2
+          ? p1
+          : !p1
+          ? p2
+          : typeIntersection(
+              p1,
+              p2,
+              title + upperFirst(camelCase(key)),
+              schemas
+            ),
       ];
     })
   );
+};
+
+const getObject = (object, schemas) => {
+  if (object.$ref) {
+    return schemas[object.$ref.split("/").at(-1)];
+  }
+  return object;
 };
 
 const typeIntersection = (p1, p2, title, schemas) => {
@@ -145,45 +213,61 @@ const typeIntersection = (p1, p2, title, schemas) => {
   if (!p2) {
     return p2;
   }
+  if (p1?.$ref && p2?.$ref && isDeepStrictEqual(p1, p2)) {
+    return p1;
+  }
   const p1Local = p1?.$ref ? schemas[p1.$ref.split("/").at(-1)] : p1;
   const p2Local = p2?.$ref ? schemas[p2.$ref.split("/").at(-1)] : p2;
   if (p1Local.oneOf && p2Local.oneOf) {
-    console.log(1);
     return mergeAllOfObjects(
-      [...p1Local.oneOf, ...p2Local.oneOf],
+      [
+        ...p1Local.oneOf.map((object) => getObject(object, schemas)),
+        ...p2Local.oneOf.map((object) => getObject(object, schemas)),
+      ],
       schemas,
       title
     );
   }
   if (p1Local.oneOf) {
     if (p2Local.allOf) {
-      console.log(2);
       return mergeAllOfObjects(
-        [...p1Local.oneOf, ...p2Local.allOf],
+        [
+          ...p1Local.oneOf.map((object) => getObject(object, schemas)),
+          ...p2Local.allOf.map((object) => getObject(object, schemas)),
+        ],
         schemas,
         title
       );
     }
-    console.log(4);
-    return mergeAllOfObjects([...p1Local.oneOf, p2Local], schemas, title);
+    return mergeAllOfObjects(
+      [...p1Local.oneOf.map((object) => getObject(object, schemas)), p2Local],
+      schemas,
+      title
+    );
   }
   if (p2Local.oneOf) {
     if (p1Local.allOf) {
-      console.log(3);
       return mergeAllOfObjects(
-        [...p2Local.oneOf, ...p1Local.allOf],
+        [
+          ...p2Local.oneOf.map((object) => getObject(object, schemas)),
+          ...p1Local.allOf.map((object) => getObject(object, schemas)),
+        ],
         schemas,
         title
       );
     }
-    console.log(5);
-    return mergeAllOfObjects([...p2Local.oneOf, p1Local], schemas, title);
+    return mergeAllOfObjects(
+      [...p2Local.oneOf.map((object) => getObject(object, schemas)), p1Local],
+      schemas,
+      title
+    );
   }
   if (
     (p1Local.allOf || p1Local.type === "object" || p1Local.properties) &&
     (p2Local.allOf || p2Local.type === "object" || p2Local.properties)
   ) {
     const allOf = [p1Local.allOf || p1Local, p2Local.allOf || p2Local].flat();
+    //nie tu
     return mergeAllOfObjects(allOf, schemas, title);
   }
   if (isDeepStrictEqual(p1, p2)) {
@@ -220,11 +304,6 @@ const typeIntersection = (p1, p2, title, schemas) => {
     );
   }
   if (p1Local.type === "array") {
-    console.log(
-      p1Local.items,
-      p2Local.items,
-      typeIntersection(p1Local.items, p2Local.items, title + "Item", schemas)
-    );
     return {
       type: "array",
       items: typeIntersection(
@@ -242,7 +321,10 @@ const mergeAllOfObjects = (allAreObjects, schemas, title) => {
   if (allAreObjects.length === 1) {
     return allAreObjects[0];
   }
-  const areAllObjectsObjects = allAreObjects.reduce(
+  const allAreObjectsLocal = allAreObjects.map((object) =>
+    getObject(object, schemas)
+  );
+  const areAllObjectsObjects = allAreObjectsLocal.reduce(
     (accumulator, currentValue) => {
       if (currentValue?.type === "object" || currentValue?.properties) {
         return accumulator;
@@ -252,53 +334,52 @@ const mergeAllOfObjects = (allAreObjects, schemas, title) => {
     true
   );
   if (areAllObjectsObjects) {
-    const result = {
-      type: "object",
-      title,
-      properties: allAreObjects[0].properties || {},
-      required: allAreObjects[0].required || [],
-    };
-
-    allAreObjects.slice(1).forEach((object) => {
-      const objectProperties = object.properties || {};
-      const objectRequired = object.required || [];
-      result.properties = propertiesIntersection(
-        result.properties,
-        objectProperties,
+    const result = allAreObjectsLocal.reduce((accumulator, currentValue) => {
+      if (!accumulator) {
+        return {
+          type: "object",
+          title,
+          properties: currentValue?.properties || {},
+          required: currentValue?.required || [],
+        };
+      }
+      const properties = propertiesIntersection(
+        accumulator.properties,
+        currentValue?.properties || {},
         title,
         schemas
       );
-      result.required = intersection(result.required, objectRequired);
-    });
-    return result;
+      const required = intersection(
+        accumulator.required,
+        currentValue?.required || []
+      );
+      return { ...accumulator, properties, required };
+    }, undefined);
+    return omit(
+      result,
+      compact([
+        result.required.length === 0 ? "required" : undefined,
+        Object.keys(result.properties).length === 0 ? "properties" : undefined,
+      ])
+    );
   }
   if (
-    !allAreObjects
+    !allAreObjectsLocal
       .slice(1)
       .reduce(
         (accumulator, currentValue) =>
           typeIntersection(accumulator, currentValue, title, schemas),
-        allAreObjects[0]
+        allAreObjectsLocal[0]
       )
   ) {
-    console.log(123, allAreObjects);
+    console.log(123, allAreObjectsLocal);
   }
-  // console.log(
-  //   123,
-  //   allAreObjects
-  //     .slice(1)
-  //     .reduce(
-  //       (accumulator, currentValue) =>
-  //         typeIntersection(accumulator, currentValue, title, schemas),
-  //       allAreObjects[0]
-  //     )
-  // );
-  return allAreObjects
+  return allAreObjectsLocal
     .slice(1)
     .reduce(
       (accumulator, currentValue) =>
         typeIntersection(accumulator, currentValue, title, schemas),
-      allAreObjects[0]
+      allAreObjectsLocal[0]
     );
 };
 
@@ -307,10 +388,14 @@ export const removeAllOneOfs = (schemas) => {
   let result;
   let index = 0;
   do {
-    result = removeSingleOneOf(localSchemas);
+    result = removeSingleOneOf({ ...localSchemas });
+    if (JSON.stringify(localSchemas) === JSON.stringify(result.schemas)) {
+      break;
+    }
+
     localSchemas = result.schemas;
     index += 1;
     console.log(index);
-  } while (result.removed);
+  } while (true);
   return localSchemas;
 };
