5ce40a5ad2c88b3090520441b01b7c81a1ca3148
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2024-01-04 12:31:48 +0100

    done

---
+++ scripts/fix-schemas-properties-with-single-enum.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import fsPromises from "fs/promises";
+
+const fixSchemasPropertiesWithSingleEnum = (object: any) => {
+  if (Array.isArray(object)) {
+    return object.map((value) => fixSchemasPropertiesWithSingleEnum(value));
+  }
+  if (object instanceof Object) {
+    if ("enum" in object && object["enum"]?.length === 1) {
+      const indexOfEnum = Object.keys(object).findIndex(
+        (value) => value === "enum"
+      );
+      const enumEntries = Object.entries(object);
+
+      return Object.fromEntries([
+        ...enumEntries.slice(0, indexOfEnum),
+        [["default"], object["enum"][0]],
+        ...enumEntries.slice(indexOfEnum),
+      ]);
+    }
+    return Object.fromEntries(
+      Object.entries(object).map((keyAndEntry) => {
+        const [key, entry] = keyAndEntry;
+        return [key, fixSchemasPropertiesWithSingleEnum(entry)];
+      })
+    );
+  }
+  return object;
+};
+const main = async () => {
+  const openApiPath = path.join(__dirname, "../reference/OpenAPI.json");
+
+  const getOpenAPI = async () =>
+    JSON.parse((await fsPromises.readFile(openApiPath)).toString());
+
+  await fsPromises.writeFile(
+    openApiPath,
+    JSON.stringify(
+      fixSchemasPropertiesWithSingleEnum(await getOpenAPI()),
+      null,
+      2
+    )
+  );
+};
+
+main();
