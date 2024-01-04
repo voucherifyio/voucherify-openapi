007accffde8adf81dcae539bde2cfdecda126326
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2024-01-04 12:11:17 +0100

    schema fix, java test

--- sdk-tests/java/src/main/java/org/example/Main.java
+++ sdk-tests/java/src/main/java/org/example/Main.java
@@ -8,8 +8,37 @@ import org.openapitools.client.api.CampaignsApi;
 import org.openapitools.client.auth.ApiKeyAuth;
 import org.openapitools.client.model.*;
 
+import java.math.BigDecimal;
+
 
 public class Main {
+    // function to generate a random string of length n
+    static String getAlphaNumericString(int n)
+    {
+
+        // choose a Character random from this String
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+
+        // create StringBuffer size of AlphaNumericString
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index
+                    = (int)(AlphaNumericString.length()
+                    * Math.random());
+
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
@@ -25,7 +54,34 @@ public class Main {
 
         CampaignsApi apiInstance = new CampaignsApi(defaultClient);
 
-        String campaignId = "camp_dMnwEX60drFdQPoNHevJbvM9";
+        CampaignsCreateRequestBody campaignsCreateRequestBody = new CampaignsCreateRequestBody(); // CampaignsCreateRequestBody | Specify the details of the campaign that you would like to create.
+        CampaignsCreateDiscountCouponsCampaign campaignsCreateDiscountCouponsCampaign = new CampaignsCreateDiscountCouponsCampaign();
+        String generatedString = Main.getAlphaNumericString(20);
+        campaignsCreateDiscountCouponsCampaign.setCampaignType("DISCOUNT_COUPONS");
+        campaignsCreateDiscountCouponsCampaign.setCampaignType("AUTO_UPDATE");
+        campaignsCreateDiscountCouponsCampaign.setName(generatedString);
+        DiscountCouponsCampaignVoucher discountCouponsCampaignVoucher = new DiscountCouponsCampaignVoucher();
+        Discount discount = new Discount();
+        DiscountAmount discountAmount = new DiscountAmount();
+        discountAmount.setType(DiscountAmount.TypeEnum.AMOUNT);
+        discountAmount.setAmountOff(BigDecimal.valueOf(1));
+        discount.setActualInstance(discountAmount);
+        discountCouponsCampaignVoucher.setDiscount(discount);
+        campaignsCreateDiscountCouponsCampaign.setVoucher(discountCouponsCampaignVoucher);
+        campaignsCreateRequestBody.setActualInstance(campaignsCreateDiscountCouponsCampaign);
+        String campaignId = "";
+
+        try {
+            CampaignsCreateResponseBody result = apiInstance.createCampaign(campaignsCreateRequestBody);
+            campaignId = result.getId();
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling CampaignsApi#createCampaign");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
 
         try {
             CampaignsGetResponseBody result = apiInstance.getCampaign(campaignId);
@@ -38,9 +94,22 @@ public class Main {
             e.printStackTrace();
         }
 
-        Integer vouchersCount = 1; // Integer | Number of vouchers that should be added.
-        CampaignsVouchersCreateInBulkRequestBody campaignsVouchersCreateInBulkRequestBody = new CampaignsVouchersCreateInBulkRequestBody(); // CampaignsVouchersCreateInBulkRequestBody | Specify the voucher parameters that you would like to overwrite.
         try {
+            Integer vouchersCount = 1; // Integer | Number of vouchers that should be added.
+            CampaignsVouchersCreateInBulkRequestBody campaignsVouchersCreateInBulkRequestBody = new CampaignsVouchersCreateInBulkRequestBody(); // CampaignsVouchersCreateInBulkRequestBody | Specify the voucher parameters that you would like to overwrite.
+            CampaignsVouchersCreateCombinedResponseBody result = apiInstance.addVouchersToCampaign(campaignId, vouchersCount, campaignsVouchersCreateInBulkRequestBody);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling CampaignsApi#addVouchersToCampaign");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+
+        try {
+            Integer vouchersCount = 2; // Integer | Number of vouchers that should be added.
+            CampaignsVouchersCreateInBulkRequestBody campaignsVouchersCreateInBulkRequestBody = new CampaignsVouchersCreateInBulkRequestBody(); // CampaignsVouchersCreateInBulkRequestBody | Specify the voucher parameters that you would like to overwrite.
             CampaignsVouchersCreateCombinedResponseBody result = apiInstance.addVouchersToCampaign(campaignId, vouchersCount, campaignsVouchersCreateInBulkRequestBody);
             System.out.println(result);
         } catch (ApiException e) {
