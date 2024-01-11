3bb381e6613e2dbb392e350771d4d40605df886d
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2024-01-11 16:59:25 +0100

    Update index.php

--- sdk-tests/php/index.php
+++ sdk-tests/php/index.php
@@ -30,6 +30,27 @@ $campaignsApiInstance = new OpenAPI\Client\Api\CampaignsApi(
     $config
 );
 
+//create campaignsApi
+$productsApiInstance = new OpenAPI\Client\Api\ProductsApi(
+    new GuzzleHttp\Client(),
+    $config
+);
+
+//create variable to store product data
+$created_product;
+//create Product
+$products_create_request_body = new \OpenAPI\Client\Model\ProductsCreateRequestBody();
+$products_create_request_body->setSourceId(generateRandomString());
+$products_create_request_body->setName(generateRandomString());
+$products_create_request_body->setPrice(20000);
+$products_create_request_body->setAttributes(["color", "memory", "processor"]);
+try {
+    $created_product = $productsApiInstance->createProduct($products_create_request_body);
+    echo '<pre>createProduct<br />' . json_encode($created_product, JSON_PRETTY_PRINT) . '</pre>';
+} catch (Exception $e) {
+    echo 'Exception when calling ProductsApi->createProduct: ', $e->getMessage(), PHP_EOL;
+}
+
 //create variable to store campaign data
 $created_discount_campaign;
 //createCampaign - DISCOUNT_COUPONS
@@ -51,11 +72,6 @@ try {
 
 //create variable to store campaign data
 $created_promotion_campaign;
-//createCampaign - PROMOTION
-$campaigns_create_request_body_promotion = new \OpenAPI\Client\Model\CampaignsCreateRequestBody();
-$campaigns_create_request_body_promotion->setName(generateRandomString(12));
-$campaigns_create_request_body_promotion->setCampaignType("PROMOTION");
-$campaigns_create_request_body_promotion->setPromotion(new \OpenAPI\Client\Model\CampaignsCreateRequestBodyPromotion());
 //promotionTierCreateParams
 $promotionTierCreateParams = new \OpenAPI\Client\Model\PromotionTierCreateParams();
 $promotionTierCreateParams->setName(generateRandomString());
@@ -64,8 +80,12 @@ $promotionTierCreateParams->setAction(new \OpenAPI\Client\Model\PromotionTierAct
 $promotionTierCreateParams->getAction()->setDiscount(new \OpenAPI\Client\Model\Discount());
 $promotionTierCreateParams->getAction()->getDiscount()->setType("AMOUNT");
 $promotionTierCreateParams->getAction()->getDiscount()->setAmountOff(1000);
+//createCampaign - PROMOTION
+$campaigns_create_request_body_promotion = new \OpenAPI\Client\Model\CampaignsCreateRequestBody();
+$campaigns_create_request_body_promotion->setName(generateRandomString(12));
+$campaigns_create_request_body_promotion->setCampaignType("PROMOTION");
+$campaigns_create_request_body_promotion->setPromotion(new \OpenAPI\Client\Model\CampaignsCreateRequestBodyPromotion());
 $campaigns_create_request_body_promotion->getPromotion()->setTiers([$promotionTierCreateParams]);
-
 try {
     $created_promotion_campaign = $campaignsApiInstance->createCampaign($campaigns_create_request_body_promotion);
     echo '<pre>createCampaign - PROMOTION<br />' . json_encode($created_promotion_campaign, JSON_PRETTY_PRINT) . '</pre>';
@@ -118,6 +138,12 @@ $created_customer;
 //createCustomer
 $customersCreateRequestBody = new \OpenAPI\Client\Model\CustomersCreateRequestBody();
 $customersCreateRequestBody->setSourceId('test123');
+$customersCreateRequestBody->setName('test123');
+$customersCreateRequestBody->setAddress(new \OpenAPI\Client\Model\CustomerBaseAddress());
+$customersCreateRequestBody->getAddress()->setCountry('US');
+$customersCreateRequestBody->getAddress()->setCity('Vice City');
+$customersCreateRequestBody->getAddress()->setLine1('123');
+$customersCreateRequestBody->getAddress()->setPostalCode('60089');
 try {
     $created_customer = $customersApiInstance->createCustomer($customersCreateRequestBody);
     echo '<pre>createCustomer<br />' . json_encode($created_customer, JSON_PRETTY_PRINT) . '</pre>';
@@ -134,13 +160,20 @@ $qualificationsApiInstance = new OpenAPI\Client\Api\QualificationsApi(
 //create variable to store checkEligibility result data
 $check_eligibility_result;
 
+
+//create orderItem
+$order_item = new \OpenAPI\Client\Model\OrderItem();
+$order_item->setPrice($created_product->getPrice());
+$order_item->setQuantity(3);
+$order_item->setProductId($created_product->getId());
+
 //qualifications_check_eligibility_request_body
 $qualifications_check_eligibility_request_body = new \OpenAPI\Client\Model\QualificationsCheckEligibilityRequestBody();
 $qualifications_check_eligibility_request_body->setCustomer(new \OpenAPI\Client\Model\Customer());
 $qualifications_check_eligibility_request_body->getCustomer()->setId($created_customer->getId());
 $qualifications_check_eligibility_request_body->setOrder(new \OpenAPI\Client\Model\Order());
-$qualifications_check_eligibility_request_body->getOrder()->setAmount(100000);
 $qualifications_check_eligibility_request_body->getOrder()->setStatus("CREATED");
+$qualifications_check_eligibility_request_body->getOrder()->setItems([$order_item]);
 $qualifications_check_eligibility_request_body->setMode("BASIC");
 $qualifications_check_eligibility_request_body->setScenario("ALL");
 try {
@@ -156,16 +189,17 @@ $applicable_promotion_tiers_ids = array_map(function ($promotion_tier) {
     return $promotion_tier->getId();
 }, $applicable_promotion_tiers);
 
-//create redemptionsApi
+//create stackableDiscountsApi
 $stackedDiscountsApiInstance = new OpenAPI\Client\Api\StackableDiscountsApi(
     new GuzzleHttp\Client(),
     $config
 );
 
+//validateStackedDiscounts
 $validations_validate_request_body = new \OpenAPI\Client\Model\ValidationsValidateRequestBody();
 $validations_validate_request_body->setOrder(new \OpenAPI\Client\Model\Order());
-$validations_validate_request_body->getOrder()->setAmount(100000);
 $validations_validate_request_body->getOrder()->setStatus("CREATED");
+$validations_validate_request_body->getOrder()->setItems([$order_item]);
 $validations_validate_request_body->setCustomer(new \OpenAPI\Client\Model\Customer());
 $validations_validate_request_body->getCustomer()->setSourceId($created_customer->getSourceId());
 $validations_validate_request_body_redeemables = [];
@@ -181,9 +215,29 @@ $voucher_redeemable->setObject("voucher");
 array_push($validations_validate_request_body_redeemables, $voucher_redeemable);
 $validations_validate_request_body->setRedeemables($validations_validate_request_body_redeemables);
 try {
-    echo '<pre>validateStackedDiscounts<br />' . json_encode($validations_validate_request_body, JSON_PRETTY_PRINT) . '</pre>';
     $result = $stackedDiscountsApiInstance->validateStackedDiscounts($validations_validate_request_body);
     echo '<pre>validateStackedDiscounts<br />' . json_encode($result, JSON_PRETTY_PRINT) . '</pre>';
+    $result->getOrder();
+    echo '<pre>order.amount = ' . $result->getOrder()->getAmount() . '<br />order.total_discount_amount = ' . $result->getOrder()->getTotalDiscountAmount()
+        . '<br />order.total_amount = ' . $result->getOrder()->getTotalAmount() . '</pre>';
+} catch (Exception $e) {
+    echo 'Exception when calling StackableDiscountsApi->redeemStackedDiscounts: ', $e->getMessage(), PHP_EOL;
+}
+
+//validateStackedDiscounts
+$redemptions_redeem_request_body = new \OpenAPI\Client\Model\RedemptionsRedeemRequestBody();
+$redemptions_redeem_request_body->setOrder(new \OpenAPI\Client\Model\Order());
+$redemptions_redeem_request_body->getOrder()->setStatus("CREATED");
+$redemptions_redeem_request_body->getOrder()->setItems([$order_item]);
+$redemptions_redeem_request_body->setCustomer(new \OpenAPI\Client\Model\Customer());
+$redemptions_redeem_request_body->getCustomer()->setSourceId($created_customer->getSourceId());
+$redemptions_redeem_request_body->setRedeemables($validations_validate_request_body_redeemables);
+try {
+    $result = $stackedDiscountsApiInstance->redeemStackedDiscounts($redemptions_redeem_request_body);
+    echo '<pre>redeemStackedDiscounts<br />' . json_encode($result, JSON_PRETTY_PRINT) . '</pre>';
+    $result->getOrder();
+    echo '<pre>order.amount = ' . $result->getOrder()->getAmount() . '<br />order.total_discount_amount = ' . $result->getOrder()->getTotalDiscountAmount()
+        . '<br />order.total_amount = ' . $result->getOrder()->getTotalAmount() . '</pre>';
 } catch (Exception $e) {
     echo 'Exception when calling StackableDiscountsApi->redeemStackedDiscounts: ', $e->getMessage(), PHP_EOL;
 }
@@ -225,4 +279,12 @@ try {
     echo '<pre>deleteCustomer<br />' . json_encode($result, JSON_PRETTY_PRINT) . '</pre>';
 } catch (Exception $e) {
     echo 'Exception when calling CustomersApi->createCustomer: ', $e->getMessage(), PHP_EOL;
+}
+
+//deleteProduct
+try {
+    $productsApiInstance->deleteProduct($created_product->getId(), true);
+    echo '<pre>deleteProduct<br />' . json_encode($result, JSON_PRETTY_PRINT) . '</pre>';
+} catch (Exception $e) {
+    echo 'Exception when calling ProductsApi->deleteProduct: ', $e->getMessage(), PHP_EOL;
 }
