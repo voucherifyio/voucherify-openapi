953ff9e5b6244a4b48abdb748dc75cd679f89526
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2024-01-11 12:43:15 +0100

    in progress

--- sdk-tests/php/index.php
+++ sdk-tests/php/index.php
@@ -1,5 +1,5 @@
 <?php
-
+//generateRandomString
 function generateRandomString($length = 10) {
     $randomString = '';
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -13,10 +13,12 @@ function generateRandomString($length = 10) {
     return $randomString;
 }
 
+//load SDK
 require_once(__DIR__ . '/vendor/autoload.php');
+//load .env
 $env = parse_ini_file('.env');
 
-// Configure
+//configure SDK
 $config = OpenAPI\Client\Configuration::getDefaultConfiguration()->setApiKey('X-App-Id', $env["X_APP_ID"]);
 $config = OpenAPI\Client\Configuration::getDefaultConfiguration()->setApiKey('X-App-Token', $env["X_APP_TOKEN"]);
 $config = OpenAPI\Client\Configuration::getDefaultConfiguration()->setHost($env["VOUCHERIFY_HOST"]);
@@ -28,25 +30,49 @@ $campaignsApiInstance = new OpenAPI\Client\Api\CampaignsApi(
 );
 
 //create variable to store campaign data
-$created_campaign;
-//createCampaign
-$campaigns_create_request_body = new \OpenAPI\Client\Model\CampaignsCreateRequestBody();
-$campaigns_create_request_body->setName(generateRandomString(12));
-$campaigns_create_request_body->setCampaignType("DISCOUNT_COUPONS");
-$campaigns_create_request_body->setVoucher(new \OpenAPI\Client\Model\CampaignsCreateRequestBodyVoucher());
-$campaigns_create_request_body->getVoucher()->setType("DISCOUNT_VOUCHER");
-$campaigns_create_request_body->getVoucher()->setDiscount(new \OpenAPI\Client\Model\Discount());
-$campaigns_create_request_body->getVoucher()->getDiscount()->setType("AMOUNT");
-$campaigns_create_request_body->getVoucher()->getDiscount()->setAmountOff(1000);
+$created_discount_campaign;
+//createCampaign - DISCOUNT_COUPONS
+$campaigns_create_request_body_discount = new \OpenAPI\Client\Model\CampaignsCreateRequestBody();
+$campaigns_create_request_body_discount->setName(generateRandomString(12));
+$campaigns_create_request_body_discount->setCampaignType("DISCOUNT_COUPONS");
+$campaigns_create_request_body_discount->setType("AUTO_UPDATE");
+$campaigns_create_request_body_discount->setVoucher(new \OpenAPI\Client\Model\CampaignsCreateRequestBodyVoucher());
+$campaigns_create_request_body_discount->getVoucher()->setType("DISCOUNT_VOUCHER");
+$campaigns_create_request_body_discount->getVoucher()->setDiscount(new \OpenAPI\Client\Model\Discount());
+$campaigns_create_request_body_discount->getVoucher()->getDiscount()->setType("AMOUNT");
+$campaigns_create_request_body_discount->getVoucher()->getDiscount()->setAmountOff(1000);
 try {
-    $created_campaign = $campaignsApiInstance->createCampaign($campaigns_create_request_body);
-    echo '<pre>createCampaign<br />' . json_encode($created_campaign, JSON_PRETTY_PRINT) . '</pre>';
+    $created_discount_campaign = $campaignsApiInstance->createCampaign($campaigns_create_request_body_discount);
+    echo '<pre>createCampaign - DISCOUNT_COUPONS<br />' . json_encode($created_discount_campaign, JSON_PRETTY_PRINT) . '</pre>';
 } catch (Exception $e) {
     echo 'Exception when calling CampaignsApi->createCampaign: ', $e->getMessage(), PHP_EOL;
 }
 
+//create variable to store campaign data
+$created_promotion_campaign;
+//createCampaign - PROMOTION
+$campaigns_create_request_body_promotion = new \OpenAPI\Client\Model\CampaignsCreateRequestBody();
+$campaigns_create_request_body_promotion->setName(generateRandomString(12));
+$campaigns_create_request_body_promotion->setCampaignType("PROMOTION");
+//promotionTierCreateParams
+$promotionTierCreateParams = new \OpenAPI\Client\Model\PromotionTierCreateParams();
+$promotionTierCreateParams->setBanner('testBanner');
+$promotionTierCreateParams->setAction(new \OpenAPI\Client\Model\PromotionTierAction());
+$promotionTierCreateParams->getAction()->setDiscount(new \OpenAPI\Client\Model\Discount());
+$promotionTierCreateParams->getAction()->getDiscount()->setType("AMOUNT");
+$promotionTierCreateParams->getAction()->getDiscount()->setAmountOff(1000);
+$campaigns_create_request_body_promotion->setPromotion([$promotionTierCreateParams]);
+try {
+    echo '<pre>createCampaign - PROMOTION<br />' . json_encode($campaigns_create_request_body_promotion, JSON_PRETTY_PRINT) . '</pre>';
+    $created_promotion_campaign = $campaignsApiInstance->createCampaign($campaigns_create_request_body_promotion);
+    echo '<pre>createCampaign - PROMOTION<br />' . json_encode($created_discount_campaign, JSON_PRETTY_PRINT) . '</pre>';
+} catch (Exception $e) {
+    echo 'Exception when calling CampaignsApi->createCampaign: ', $e->getMessage(), PHP_EOL;
+}
+
+
 //listCampaigns
-$limit = 1; // int | A limit on the number of objects to be returned. Limit can range between 1 and 100 items.
+$limit = 2; // int | A limit on the number of objects to be returned. Limit can range between 1 and 100 items.
 $page = 1; // int | Which page of results to return.
 $campaign_type = \OpenAPI\Client\Model\ParameterCampaignType::DISCOUNT_COUPONS->value; // ParameterCampaignType
 $expand = \OpenAPI\Client\Model\ParameterExpandListCampaigns::CATEGORY->value; // ParameterExpandListCampaigns
@@ -60,9 +86,19 @@ try {
 
 //getCampaign
 try {
-    $result = $campaignsApiInstance->getCampaign($created_campaign->getId());
+    $result = $campaignsApiInstance->getCampaign($created_discount_campaign->getId());
     echo '<pre>getCampaign<br />' . json_encode($result, JSON_PRETTY_PRINT) . '</pre>';
     echo '<pre>getValidationRulesAssignments<br />' . json_encode($result->getValidationRulesAssignments(), JSON_PRETTY_PRINT) . '</pre>';
+} catch (Exception $e) {
+    echo 'Exception when calling CampaignsApi->getCampaign: ', $e->getMessage(), PHP_EOL;
+}
+
+
+//createVoucher
+$voucher;
+try {
+    $voucher = $campaignsApiInstance->addVouchersToCampaign($created_discount_campaign->getId(),1);
+    echo '<pre>addVouchersToCampaign<br />' . json_encode($voucher, JSON_PRETTY_PRINT) . '</pre>';
 } catch (Exception $e) {
     echo 'Exception when calling CampaignsApi->getCampaign: ', $e->getMessage(), PHP_EOL;
 }
@@ -74,28 +110,25 @@ $customersApiInstance = new OpenAPI\Client\Api\CustomersApi(
 );
 
 //create variable to store customer data
-$created_campaign;
+$created_customer;
 
 //createCustomer
 $customersCreateRequestBody = new \OpenAPI\Client\Model\CustomersCreateRequestBody();
 $customersCreateRequestBody->setSourceId('test123');
-
 try {
-    $result = $customersApiInstance->createCustomer($customersCreateRequestBody);
-     echo '<pre>createCustomer<br />' . json_encode($result, JSON_PRETTY_PRINT) . '</pre>';
+    $created_customer = $customersApiInstance->createCustomer($customersCreateRequestBody);
+     echo '<pre>createCustomer<br />' . json_encode($created_customer, JSON_PRETTY_PRINT) . '</pre>';
 } catch (Exception $e) {
     echo 'Exception when calling CustomersApi->createCustomer: ', $e->getMessage(), PHP_EOL;
 }
 
-
-
-
-
+//create redemptionsApi
 $redemptionsApiInstance = new OpenAPI\Client\Api\RedemptionsApi(
     new GuzzleHttp\Client(),
     $config
 );
 
+//listRedemptions
 try {
     $result = $redemptionsApiInstance->listRedemptions(1, 1);
      echo '<pre>listRedemptions<br />' . json_encode($result, JSON_PRETTY_PRINT) . '</pre>';
@@ -104,12 +137,29 @@ try {
 }
 
 
-
-//delete campaign
+//deleteCampaign - DISCOUNT_COUPONS
 try {
-    $result = $campaignsApiInstance->deleteCampaign($created_campaign->getId(), true);
-    echo '<pre>deleteCampaign<br />' . json_encode($result, JSON_PRETTY_PRINT) . '</pre>';
+    $result = $campaignsApiInstance->deleteCampaign($created_discount_campaign->getId(), true);
+    echo '<pre>deleteCampaign - DISCOUNT_COUPONS<br />' . json_encode($result, JSON_PRETTY_PRINT) . '</pre>';
     echo '<pre>getAsyncActionId<br />' . $result->getAsyncActionId() . '</pre>';
 } catch (Exception $e) {
     echo 'Exception when calling CampaignsApi->listCampaigns: ', $e->getMessage(), PHP_EOL;
+}
+
+//deleteCampaign - PROMOTION
+try {
+    $result = $campaignsApiInstance->deleteCampaign($created_promotion_campaign->getId(), true);
+    echo '<pre>deleteCampaign - PROMOTION<br />' . json_encode($result, JSON_PRETTY_PRINT) . '</pre>';
+    echo '<pre>getAsyncActionId<br />' . $result->getAsyncActionId() . '</pre>';
+} catch (Exception $e) {
+    echo 'Exception when calling CampaignsApi->listCampaigns: ', $e->getMessage(), PHP_EOL;
+}
+
+
+//deleteCustomer
+try {
+    $result = $customersApiInstance->deleteCustomer($created_customer->getId());
+     echo '<pre>createCustomer<br />' . json_encode($result, JSON_PRETTY_PRINT) . '</pre>';
+} catch (Exception $e) {
+    echo 'Exception when calling CustomersApi->createCustomer: ', $e->getMessage(), PHP_EOL;
 }
