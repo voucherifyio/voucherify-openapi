f6f3fced88ebd183a114c91684d3cbb208c92c1d
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2024-01-17 08:23:45 +0100

    1st commit

--- sdk-tests/php/copySdk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ./tmp/php || exit
-composer install
-cd ../../ || exit
-cp -r ./tmp/php/vendor ./sdk-tests/php
-cp -r ./tmp/php/src ./sdk-tests/php
-cp -r ./tmp/php/tests ./sdk-tests/php
