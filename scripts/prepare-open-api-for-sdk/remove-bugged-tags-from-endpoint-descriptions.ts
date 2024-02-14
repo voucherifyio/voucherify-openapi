3b5496355c19552e6564d5128f21c2dc457ee7e1
jkaliszuk <jakub.kaliszuk@rspective.pl>
2024-02-14 12:42:36 +0100

    Reduced complicated descriptions from Ruby API docs.

--- scripts/prepare-open-api-for-sdk/remove-bugged-tags-from-endpoint-descriptions.ts
+++ scripts/prepare-open-api-for-sdk/remove-bugged-tags-from-endpoint-descriptions.ts
@@ -2,15 +2,54 @@ export const removeBuggedTagsFromEndpointDescriptions = (paths: any) => {
 
     Object.keys(paths).forEach(key => {
         Object.keys(paths[key]).forEach(method => {
-            const description = paths[key][method].description?.replace(/<!--[\s\S]*?-->/g, '') ?? null;
+            let description = paths[key][method].description
 
-            if(description){
-                paths[key][method].description = description
+            if(!description){
+                return
             }
+
+            description = removeUnnecessarilyHtmlTag(description);
+            description = removeTable(description);
+            description = reduceMultipleAndWrongNewLineElements(description);
+            description = removeLinks(description);
+
+
+            paths[key][method].description = description
         })
     })
 
-    console.log(paths)
-
     return paths
+}
+
+const removeUnnecessarilyHtmlTag = (description: string) => {
+    return description
+        .replace(/<!--[\s\S]*?-->/g, '')
+        .replace(/##/g, '#');
+}
+
+const removeTable = (description: string) => {
+    return description.replace(/\|.*\|/g, '')
+}
+
+const reduceMultipleAndWrongNewLineElements = (description: string) => {
+    return description
+        .replace(/(?<!\r)\n\|/g, '\n')
+        .replace(/(?<!\r)\n>/g, '\n')
+        .replace(/(?<!\r)\n\n\n/g, '\n')
+        .replace(/(?<!\r)\n\n/g, '\n')
+        .replace(/(?<!\r)\n/g, '\n');
+}
+
+const removeLinks = (description: string) => {
+    const matches = description.match(/\[(.*?)\]\(ref:(.*?)\)/g)
+
+    if(matches && matches.length > 0){
+        matches.forEach((match: any) => {
+
+            const replacement = match.match(/\[([^\]]+)\]/);
+            description = description.replace(match, replacement ? replacement[1] : '')
+        })
+    }
+
+    return description
 }
