1c4f0a1d4d59d85dcc027360ee3af690a38cbe59
p-zielinski <piotrzielinski96@yahoo.com>
2025-09-17 12:31:41 +0200

    test - objects

--- scripts/build-update-md-tables-from-openapi.ts
+++ scripts/build-update-md-tables-from-openapi.ts
@@ -8,9 +8,13 @@ import SchemaToMarkdownTable, {
   RenderMode,
 } from "./src/schema-to-md-table";
 import { EOL } from "os";
+import _ from "lodash";
+import { prettify } from "htmlfy";
 
-const PATH_TO_GERENATED_TABLES = [__dirname, "./output"];
-const PATH_TO_DOCS_REFERENCE = [__dirname, "../docs/reference-docs"];
+const PATH_TO_DOCS_REFERENCE = [
+  __dirname,
+  "../documentation/markdowns/objects",
+];
 const PATH_TO_GENERATED_TABLES = [__dirname, "./output"];
 
 export const buildUpdateMdTablesFromOpenapi = async () => {
@@ -25,7 +29,7 @@ export const buildUpdateMdTablesFromOpenapi = async () => {
     try {
       const fileName = `${objectName}.md`;
       await fs.writeFile(
-        path.join(...PATH_TO_GERENATED_TABLES, fileName),
+        path.join(...PATH_TO_GENERATED_TABLES, fileName),
         stm.render(objectName),
       );
       console.log(`Generated markdown table in ${fileName}`);
@@ -39,57 +43,32 @@ export const buildUpdateMdTablesFromOpenapi = async () => {
 };
 
 export const updateMdTablesInDoc = async () => {
+  const { default: markdown, getCodeString } = await import(
+    "@wcj/markdown-to-html"
+  );
+
   for (const [objectName, docFile] of mdTables) {
     if (!docFile) {
       continue;
     }
     try {
-      const docPath = path.join(...PATH_TO_DOCS_REFERENCE, docFile);
-      const fileContent = await fs.readFile(docPath);
-      const fileContentBlocks = fileContent
-        .toString()
-        .split(/(^---$)|(^\[block\:html\]$)/m) // Split by `---` and [block:html] that surrounds the table
-        .filter((e) => !!e);
+      const mdPath = path.join(...PATH_TO_GENERATED_TABLES, `${objectName}.md`);
+      console.log(mdPath);
+      const fileContent = await fs.readFile(mdPath, "utf8");
+      const fileContentAsHtml = prettify(markdown(fileContent) as string);
 
-      // Find block with table by part of the markdown table syntax
-      const contentBlockIndexWithTableToReplace = fileContentBlocks.findIndex(
-        (block) => block.indexOf("|:-----") >= 0,
-      );
-
-      if (contentBlockIndexWithTableToReplace < 0) {
-        throw new Error(
-          `Could not find table to replace in file ${docFile} (object: ${objectName}) `,
-        );
-      }
-
-      const additionalBlockquotes =
-        fileContentBlocks[contentBlockIndexWithTableToReplace].match(
-          /^\>.*$/gm,
-        );
-
-      const contentBeforeTable = fileContentBlocks
-        .slice(0, contentBlockIndexWithTableToReplace)
-        .join("");
-      const contentAfterTable = fileContentBlocks
-        .slice(contentBlockIndexWithTableToReplace + 1)
-        .join("");
-
-      const newTable = (
-        await fs.readFile(
-          path.join(...PATH_TO_GENERATED_TABLES, `${objectName}.md`),
-        )
-      ).toString();
-      // .replace((/^\# .*$/m), ''); // Remove first header as in readme.io it already exists
-
-      const newFileContent = [
-        contentBeforeTable,
-        additionalBlockquotes?.length ? additionalBlockquotes.join(EOL) : false,
-        newTable,
-        contentAfterTable,
-      ]
+      const newFileContent = _.compact([
+        `---
+title: "${docFile?.title ?? objectName}"
+mode: "frame"
+---`,
+        docFile?.htmlDescription,
+        fileContentAsHtml,
+      ])
         .filter((e) => !!e)
         .join(`${EOL}${EOL}`);
 
+      const docPath = path.join(...PATH_TO_DOCS_REFERENCE, `${objectName}.mdx`);
       await fs.writeFile(docPath, newFileContent);
       console.log(`Updated table in ${docFile} `);
     } catch (e) {
