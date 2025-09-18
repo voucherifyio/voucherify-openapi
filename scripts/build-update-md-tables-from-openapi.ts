074ea21859e8eb587ae38d88817b736296d88b7e
p-zielinski <piotrzielinski96@yahoo.com>
2025-09-18 08:16:47 +0200

    test

--- scripts/build-update-md-tables-from-openapi.ts
+++ scripts/build-update-md-tables-from-openapi.ts
@@ -64,7 +64,6 @@ export const updateMdTablesInDoc = async () => {
       const newFileContent = _.compact([
         `---
 title: "${docFile?.title ?? objectName}"
-mode: "frame"
 ---`,
         '<div class="prose custom-html">',
         docFile?.htmlDescription,
